e3bbf50f433b75d2cc38f99311948c3d8b6e8d1c
perdun <andrejnnefedov72@gmail.com>
2026-02-18 01:15:39 +0300

    Completed task 2

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import sys
-from PyQt6 import QtCore, QtGui, QtWidgets
-from PyQt6.QtWidgets import QApplication, QMainWindow, QLabel, QVBoxLayout, QWidget
-from PyQt6.QtWidgets import QMainWindow, QApplication
+from PyQt6.QtCore import Qt
+from PyQt6 import QtCore, QtWidgets
+from PyQt6.QtWidgets import QApplication, QMainWindow, QVBoxLayout, QWidget
 import requests
 from PyQt6.QtGui import QPixmap
 
 MAP_FILE = "map.png"
+
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
@@ -14,7 +15,7 @@ class Ui_MainWindow(object):
         self.centralwidget = QtWidgets.QWidget(parent=MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.label_for_map = QtWidgets.QLabel(parent=self.centralwidget)
-        self.label_for_map.setGeometry(QtCore.QRect(0, 0, 66, 18))
+        self.label_for_map.setGeometry(QtCore.QRect(0, 0, 800, 600))
         self.label_for_map.setObjectName("label_for_map")
         MainWindow.setCentralWidget(self.centralwidget)
         self.menubar = QtWidgets.QMenuBar(parent=MainWindow)
@@ -31,40 +32,47 @@ class Ui_MainWindow(object):
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
-        self.label_for_map.setText(_translate("MainWindow", "TextLabel"))
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-        self.setObjectName("wtf_pepe")
+        self.z = 18
+        self.z_dif = 1
         self.setup()
 
     def setup(self):
         self.get_image()
         central_widget = QWidget()
         self.setCentralWidget(central_widget)
-        layout = QVBoxLayout(central_widget)
-        pixmap = QPixmap('map.png')
-        self.label_for_map.setPixmap(pixmap)
-
-        layout.addWidget(self.label_for_map)
+        self.layout = QVBoxLayout(central_widget)
+        self.layout.addWidget(self.label_for_map)
 
     def get_image(self):
         api_key = 'f3a0fe3a-b07e-4840-a1da-06f18b2ddf13'
         lat = 54.7224888
         lon = 30.457209
-        spn = 0.002
         params = {
             "ll": f"{lon},{lat}",
-            "spn": f"{spn},{spn}",
+            "z": str(self.z),
             "apikey": api_key
         }
         server_address = 'https://static-maps.yandex.ru/v1?'
         response = requests.get(server_address, params=params)
         with open('map.png', "wb") as file:
             file.write(response.content)
+
+        pixmap = QPixmap('map.png')
+        self.label_for_map.setPixmap(pixmap)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key.Key_PageUpUp and self.z < 21:
+            self.z += self.z_dif
+            self.get_image()
+        elif event.key() == Qt.Key.Key_PageDown and self.z > 0:
+            self.z -= self.z_dif
+            self.get_image()
 
 
 if __name__ == "__main__":
